1b4b97bad772253cada1279d09591b9730baaf00
contact@reign-studios.com <contact@reign-studios.com@a52e07fc-d217-9b95-c1e2-eeddb2ce62b5>
2011-02-11 21:52:51 +0000

    First Commit.

---
+++ SVN_Remover/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace SVN_Remover
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+            this.selectButton = new System.Windows.Forms.Button();
+            this.applyButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dirTextBox = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.removalText = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // selectButton
+            // 
+            this.selectButton.Location = new System.Drawing.Point(59, 4);
+            this.selectButton.Name = "selectButton";
+            this.selectButton.Size = new System.Drawing.Size(99, 23);
+            this.selectButton.TabIndex = 0;
+            this.selectButton.Text = "Select Directory";
+            this.selectButton.UseVisualStyleBackColor = true;
+            this.selectButton.Click += new System.EventHandler(this.selectButton_Click);
+            // 
+            // applyButton
+            // 
+            this.applyButton.Location = new System.Drawing.Point(59, 33);
+            this.applyButton.Name = "applyButton";
+            this.applyButton.Size = new System.Drawing.Size(99, 23);
+            this.applyButton.TabIndex = 1;
+            this.applyButton.Text = "Execute Removal";
+            this.applyButton.UseVisualStyleBackColor = true;
+            this.applyButton.Click += new System.EventHandler(this.applyButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Step 1:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Step 2:";
+            // 
+            // dirTextBox
+            // 
+            this.dirTextBox.Location = new System.Drawing.Point(164, 6);
+            this.dirTextBox.Name = "dirTextBox";
+            this.dirTextBox.Size = new System.Drawing.Size(222, 20);
+            this.dirTextBox.TabIndex = 4;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.removalText);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Location = new System.Drawing.Point(15, 63);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(371, 64);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Removal Stats";
+            // 
+            // removalText
+            // 
+            this.removalText.AutoSize = true;
+            this.removalText.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.removalText.Location = new System.Drawing.Point(191, 16);
+            this.removalText.Name = "removalText";
+            this.removalText.Size = new System.Drawing.Size(35, 15);
+            this.removalText.TabIndex = 5;
+            this.removalText.Text = "None";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 16);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(179, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Number of removed .svn Directories:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(398, 141);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.dirTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.applyButton);
+            this.Controls.Add(this.selectButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TortoiseSVN Remover";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button selectButton;
+		private System.Windows.Forms.Button applyButton;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox dirTextBox;
+		private System.Windows.Forms.GroupBox groupBox1;
+		private System.Windows.Forms.Label removalText;
+		private System.Windows.Forms.Label label4;
+	}
+}
+
